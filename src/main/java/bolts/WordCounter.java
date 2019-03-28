4489cccfbda4955e067b69b8b0590455cd63af05
wonking <1434846599@qq.com>
2019-03-28 22:50:55 +0800

    计数不能用原子类

--- src/main/java/bolts/WordCounter.java
+++ src/main/java/bolts/WordCounter.java
@@ -9,7 +9,9 @@ import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -17,21 +19,34 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class WordCounter extends BaseRichBolt{
     private OutputCollector outputCollector;
-    private Map<String,AtomicInteger> count;
+    private Map<String,Integer> count;
 
 
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
         this.outputCollector=outputCollector;
         count=new HashMap<>();
+        int taskNum=topologyContext.getComponentTasks("count-bolt").size();
+        Map<Integer,String> taskToComponent=topologyContext.getTaskToComponent();
+        String componentId=topologyContext.getThisComponentId();
+        int taskId=topologyContext.getThisTaskId();
+        Set<String> streams= topologyContext.getThisStreams();
+        List<Integer> tasks= topologyContext.getThisWorkerTasks();
+        String desc=String.format("streams->%s, tasks->%s, componentId->%s, taskComponent->%s, taskId->%d",
+                streams.toString(), tasks.toString(), componentId, taskToComponent.toString(), taskId);
+        System.out.println(desc);
+        System.out.println("-----componentId->"+topologyContext.getThisComponentId()+
+                " taskId->"+topologyContext.getThisTaskId()+" taskNum->"+taskNum+"-----");
     }
 
     public void execute(Tuple tuple) {
         String s=tuple.getStringByField("word");
-        AtomicInteger c=count.get(s);
-        int nc=c==null? 0 : c.incrementAndGet();
-        AtomicInteger a=new AtomicInteger(nc);
+        Integer a=count.get(s);
+        a= a==null? 1:a+1;
+        //AtomicInteger c=count.get(s);
+        //int nc=c==null? 0 : c.incrementAndGet();
+        //AtomicInteger a=new AtomicInteger(nc);
         count.put(s, a);
-        outputCollector.emit(new Values(s,a.get()));
+        outputCollector.emit(new Values(s,a));
     }
 
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
